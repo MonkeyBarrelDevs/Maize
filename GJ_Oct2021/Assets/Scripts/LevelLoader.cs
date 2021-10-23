f0a9fee345a5859f197a7df193afa37c23f44b07
TinyDeath2 <owlman02@gmail.com>
2021-10-23 15:26:44 -0600

    Scene Manager
    adds scenes official folder and scene manager prefab and script. Also includes buttons where needed

---
+++ GJ_Oct2021/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] Animator transition;
+
+    [SerializeField] float transitionTime;
+
+    public void LoadNextLevel() 
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void ReplayTransition() 
+    {
+        StartCoroutine(CycleTransition());
+    }
+
+    public void LoadLevelAtIndex(int index)
+    {
+        StartCoroutine(LoadLevel(index));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) 
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator CycleTransition() 
+    {
+        yield return new WaitForSeconds(transitionTime);
+
+    }
+}
