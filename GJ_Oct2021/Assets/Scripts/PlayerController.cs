1d60a65e461b8169101ef678f81501490fdc7a60
Sebastien L Combes <fartingpickles@gmail.com>
2021-10-22 22:55:25 -0600

    Updated Player Movement to implement LookAt
    Player and flashlight now aims towards the mouse

---
+++ GJ_Oct2021/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Visible Fields
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed;
+
+    // Hidden Fields
+    Vector2 moveDirection;
+    bool canMove;
+
+    void Start()
+    {
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+        {
+            // 2D Movement
+            moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            rb.velocity = moveDirection * speed;
+
+            // Camera Movement
+            Vector3 mousePosition = GetMouseWorldPosition();
+
+            Vector3 aimDirection = (mousePosition - transform.position).normalized;
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            playerTransform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+
+    private static Vector3 GetMouseWorldPosition() 
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
