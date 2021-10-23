308f1a094bdcd37f0e6e1be6f1af0c56623ff492
Sebastien L Combes <fartingpickles@gmail.com>
2021-10-22 22:35:19 -0600

    Finished Mouse LookAt
    Programmed objects under "Aim" within the SampleScene to point towards the mouse cursor.

---
+++ GJ_Oct2021/Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField] Transform aimTransform; 
+    
+    private void Update() 
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private static Vector3 GetMouseWorldPosition() 
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
