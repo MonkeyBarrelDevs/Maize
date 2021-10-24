363cd63f25c8794a9cb8eb601a54bd265dd1bb63
Ethan Boggs <ethanbeez@gmail.com>
2021-10-23 18:51:26 -0600

    3 second stun functioning

---
+++ GJ_Oct2021/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MonsterController : MonoBehaviour
+{   
+    private AIPath aiPath;
+    //public bool stunned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiPath = gameObject.GetComponent<AIPath>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag.Equals("Shotgun")) {
+            StartCoroutine(StunMonster());
+            
+        }    
+    }
+    private IEnumerator StunMonster() {
+        aiPath.canMove = false;
+        yield return new WaitForSeconds(4);
+        aiPath.canMove = true;
+        
+    }
+}
